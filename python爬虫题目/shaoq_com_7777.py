91c9f389e1b7155236f0439214ddf74126e332b2
DeSireFire <1025212779@qq.com>
2019-03-12 00:58:11 +0800

    session反爬解决方案。

--- python爬虫题目/shaoq_com_7777.py
+++ python爬虫题目/shaoq_com_7777.py
@@ -1,34 +1,23 @@
-import requests,time
-
+import requests
+__author__ = 'RaXian'
 
 url = 'http://shaoq.com:7777/exam'
+req_s = requests.session()
+req = req_s.get(url=url)
+print (req.cookies.items())
+print (req.text)
+for i in range(1, 14):
+    try:
+        response = req_s.get('http://shaoq.com:7777/img/%s.png'%i, timeout=0.1)
+    except:
+        pass
+req = req_s.get(url=url)
+print (req.cookies.items())
+print (req.headers)
+print (req.text)
 
-header = {
-# 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-# 'Accept-Encoding':'gzip, deflate',
-# 'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8',
-'Cache-Control':'max-age=0',
-'Connection':'keep-alive',
-'Cookie':'session=0d6f8232d0aa1941aa483f96496d353d',
-'Host':'shaoq.com:7777',
-'If-None-Match':'"b0ff84fd9783d259d3b5433e97d90ad9585001c5"',
-'Referer':'http://shaoq.com:7777/exam',
-'Upgrade-Insecure-Requests':'1',
-'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'','
-}
-req = requests.get(url=url,headers=header)
-print(req.headers['Date'])
-print(req.headers['Etag'])
-print(req.headers['Set-Cookie'].split(';')[0])
-header['Cookie'] = req.headers['Set-Cookie'].split(';')[0]
-header['If-None-Match'] = req.headers['Etag']
-del header['Cache-Control']
-print(header)
-time.sleep(3)
-req2 = requests.get(url=url,headers=header)
-print(req2.headers['Date'])
-print(req2.text)
-print(req2.headers['Content-Length'])
+
+
 # # 查看请求结果的类型
 # print(type(req))
 # print("*"*50)
@@ -41,6 +30,7 @@ print(req2.headers['Content-Length'])
 # # 查看请求后获得cookies
 # print(type(req.cookies))
 # print(req.cookies)
+# print (req.cookies.items())
 # for i in req.cookies:
 #     print(i.name+"="+i.value)
 # print("*"*50)
