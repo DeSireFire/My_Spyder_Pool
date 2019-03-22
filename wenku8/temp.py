bb0ed704fb9c51dbd011e41830a28bd12c288708
DeSireFire <1025212779@qq.com>
2019-03-22 20:11:09 +0800

    测试摸鱼的代码

--- wenku8/temp.py
+++ wenku8/temp.py
@@ -1,6 +1,7 @@
 import requests,chardet
-url = 'http://t1.aixinxi.net/o_233qwe6667788.txt'
-resRespone = requests.get(url=url,)
+url = 'http://dl.wenku8.com/down.php?type=utf8&id=1'
+# url = 'http://t1.aixinxi.net/o_233qwe6667788.txt'
+resRespone = requests.get(url=url)
 resRespone.encoding = chardet.detect(resRespone.content)['encoding']
 if resRespone.encoding == "GB2312":
     resRespone.encoding = "GBK"
