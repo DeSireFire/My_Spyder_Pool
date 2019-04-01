01aa12c83faea5c26d8a045f784f444b273e8e10
DeSireFire <1025212779@qq.com>
2019-04-02 00:09:53 +0800

    。。说真的，这个代理池感觉不好用啊，感觉是不是要换掉比较好咧

--- proxyTest/proxyTest.py
+++ proxyTest/proxyTest.py
@@ -4,6 +4,7 @@ import requests,json
 proxyUrl1 = 'http://193.112.52.146:8000/?types=0&count=100&country=国内'
 proxyUrl2 = 'http://193.112.52.146:8000/?types=0&count=100&country=国外'
 # proxyUrl = 'http://www.zzuliacgn.com:8000/?types=0&count=10&country=%E5%9B%BD%E5%A4%96'
+# testUrl = 'https://share.dmhy.org/'
 testUrl = 'https://www.baidu.com/'
 delProxyUrl = 'http://193.112.52.146:8000/delete?ip=%s&port=%s'
 # 获取 代理列表
@@ -11,6 +12,7 @@ res = requests.get(url=proxyUrl1)
 proxyList = json.loads(res.text)
 res = requests.get(url=proxyUrl2)
 proxyList += json.loads(res.text)
+print(len(proxyList))
 for temp in proxyList:
     proxies = {
         'http': 'http://%s:%s'%(temp[0],temp[1]),
@@ -18,8 +20,9 @@ for temp in proxyList:
     }
     try:
         response = requests.get(testUrl, proxies=proxies, timeout = 10,verify=False)
+        print(response.text)
         if requests.status_codes ==200:
-            print(print(proxies))
+            print(proxies)
     except Exception as e:
         print(e)
         # if '强迫关闭' in str(e) or '积极拒绝' in str(e):
