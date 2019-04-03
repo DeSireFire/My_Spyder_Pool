eeb75cc19b69c6808235b5ff58872f28f01ed769
DeSireFire <1025212779@qq.com>
2019-04-03 21:17:29 +0800

    测试代理池ip是否可用

--- proxyTest/proxyTest.py
+++ proxyTest/proxyTest.py
@@ -1,32 +1,32 @@
 # 测试代理IP是否可用
 import requests,json
 
-proxyUrl1 = 'http://193.112.52.146:8000/?types=0&count=100&country=国内'
-proxyUrl2 = 'http://193.112.52.146:8000/?types=0&count=100&country=国外'
-# proxyUrl = 'http://www.zzuliacgn.com:8000/?types=0&count=10&country=%E5%9B%BD%E5%A4%96'
-# testUrl = 'https://share.dmhy.org/'
-testUrl = 'https://www.baidu.com/'
-delProxyUrl = 'http://193.112.52.146:8000/delete?ip=%s&port=%s'
+proxyUrl1 = 'http://192.168.37.128:5010/get_all/'
+proxyUrl2 = 'http://192.168.37.128:5010/get_all/'
+testUrl = 'https://www.wenku8.net/book/1.htm'
+# testUrl = 'http://share.dmhy.org'
+# testUrl = 'http://www.baidu.com'
+# testUrl = 'http://httpbin.org/get'
+delProxyUrl = 'http://192.168.37.128:5010/delete?proxy=%s'
 # 获取 代理列表
 res = requests.get(url=proxyUrl1)
 proxyList = json.loads(res.text)
-res = requests.get(url=proxyUrl2)
-proxyList += json.loads(res.text)
-print(len(proxyList))
+print(res.text)
 for temp in proxyList:
     proxies = {
-        'http': 'http://%s:%s'%(temp[0],temp[1]),
-        'https': 'http://%s:%s'%(temp[0],temp[1]),
+        'http': 'http://%s'%(temp),
+        'https': 'http://%s'%(temp),
     }
     try:
-        response = requests.get(testUrl, proxies=proxies, timeout = 10,verify=False)
+        response = requests.get(testUrl, proxies=proxies,verify=False)
         print(response.text)
         if requests.status_codes ==200:
             print(proxies)
     except Exception as e:
         print(e)
-        print('执行删除操作！%s' % temp[0])
-        temp = delProxyUrl % (temp[0], temp[1])
-        delproxy = requests.get(temp)
-        if int(delproxy.text):
-            print('删除成功！')
+        print('执行删除操作！%s' % temp)
+        temps = delProxyUrl % (temp)
+        print(temps)
+        # delproxy = requests.get(temps)
+        # if delproxy.text:
+        #     print('删除成功！')
