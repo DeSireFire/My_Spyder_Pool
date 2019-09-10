f7d0d725307eb8d6438740290ca18c6f086e1f9f
DeSireFire <1025212779@qq.com>
2019-09-10 15:35:36 +0800

    测试od的过滤器

---
+++ onedrive/odFiltering.py
@@ -0,0 +1,24 @@
+import requests,json
+from onedrive.authDemo import flush_token,oauthDict
+from onedrive.sec import *
+
+def odf(client):
+    '''
+    选择性查询od
+    :param client:
+    :return:
+    '''
+     # 查询文件名包含.jpg且 "image类型不为 null" 的所有子项
+    url = app_url + '/v1.0/me/drive/'+"/root/search(q='.jpg')?filter=image%20ne%20null"
+
+    headers = {'Authorization': 'bearer {}'.format(client["access_token"])}
+    get_res = requests.get(url, headers=headers)
+    get_res = json.loads(get_res.text)
+    print(get_res)
+    print(len(get_res['value']))
+    for i in get_res['value']:
+        print(i)
+
+if __name__ == '__main__':
+    temp = flush_token(info["refresh_token"])
+    odf(temp)
